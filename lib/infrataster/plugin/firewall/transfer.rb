1703a758714e660cf44cafb72fa275a1a2bd3a12
Hiroshi Ota <otahi.pub@gmail.com>
2015-04-11 14:44:28 +0900

    Enable ICMP reachable?

--- lib/infrataster/plugin/firewall/transfer.rb
+++ lib/infrataster/plugin/firewall/transfer.rb
@@ -20,7 +20,24 @@ module Infrataster
         private
 
         def icmp_reachable?
-          true
+          addresses(@dest_node).each do |a|
+            @src_node.server.ssh_exec("ping -c1 -W3 #{a}")
+          end
+        end
+
+        def addresses(node)
+          addrs = nil
+          if node.respond_to?(:server)
+            addrs = case node.server
+                    when respond_to?(:rep_address)
+                      node.server.rep_address
+                    when respond_to?(:server)
+                      node.server.address
+                    end
+          else
+            addrs = node.to_s
+          end
+          [addrs].flatten
         end
       end
     end
